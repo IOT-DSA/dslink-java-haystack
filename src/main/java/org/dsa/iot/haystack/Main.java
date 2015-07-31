a11a413a2ee22f3f2b0d5c71fd7c183534f4d093
samrg472 <samrg472@gmail.com>
2015-07-31 15:42:28 -0700

    Dynamically create actions and invoke them as necessary

--- src/main/java/org/dsa/iot/haystack/Main.java
+++ src/main/java/org/dsa/iot/haystack/Main.java
@@ -4,13 +4,23 @@ import org.dsa.iot.dslink.DSLink;
 import org.dsa.iot.dslink.DSLinkFactory;
 import org.dsa.iot.dslink.DSLinkHandler;
 import org.dsa.iot.dslink.node.Node;
+import org.dsa.iot.dslink.node.NodeBuilder;
 import org.dsa.iot.dslink.node.NodeManager;
 import org.dsa.iot.dslink.util.StringUtils;
-import org.projecthaystack.HRef;
+import org.dsa.iot.haystack.actions.Actions;
+import org.dsa.iot.haystack.actions.InvokeActions;
+import org.projecthaystack.*;
+import org.projecthaystack.client.HClient;
+import org.projecthaystack.io.HZincReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.vertx.java.core.Handler;
 
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Samuel Grenier
@@ -70,7 +80,99 @@ public class Main extends DSLinkHandler {
         return node;
     }
 
+    @Override
+    public Node onInvocationFail(final String path) {
+        final String[] split = NodeManager.splitPath(path);
+
+        final HRef id;
+        {
+            String sID = split[split.length - 2];
+            sID = StringUtils.decodeName(sID);
+            id = HRef.make(sID);
+        }
+
+        final NodeManager manager = link.getNodeManager();
+        final Node superRoot = manager.getSuperRoot();
+        final Haystack haystack = superRoot.getChild(split[0]).getMetaData();
+        final String actName = StringUtils.decodeName(split[split.length - 1]);
+
+        final CountDownLatch latch = new CountDownLatch(1);
+        final Container container = new Container();
+        switch (actName) {
+            case "pointWrite": {
+                haystack.getConnHelper().getClient(new Handler<HClient>() {
+                    @Override
+                    public void handle(HClient event) {
+                        HDict dict = event.readById(id);
+                        HVal hKind = dict.get("kind", false);
+                        String kind = null;
+                        if (hKind != null) {
+                            kind = hKind.toString();
+                        }
+
+                        String[] pSplit = Arrays.copyOf(split, split.length - 1);
+                        String parent = StringUtils.join(pSplit, "/");
+                        Node node = manager.getNode(parent, true).getNode();
+                        NodeBuilder b = node.createChild("pointWrite");
+                        b.setDisplayName("Point Write");
+                        b.setSerializable(false);
+                        b.setAction(Actions.getPointWriteAction(haystack, id, kind));
+                        container.node = b.build();
+                        latch.countDown();
+                    }
+                });
+                break;
+            }
+            default: {
+                haystack.getConnHelper().getClient(new Handler<HClient>() {
+                    @Override
+                    public void handle(HClient event) {
+                        HDict dict = event.readById(id);
+                        HVal actions = dict.get("actions");
+                        String zinc = ((HStr) actions).val;
+                        if (!zinc.endsWith("\n")) {
+                            zinc += "\n";
+                        }
+                        HZincReader reader = new HZincReader(zinc);
+                        HGrid grid = reader.readGrid();
+                        Iterator it = grid.iterator();
+                        boolean doThrow = true;
+                        while (it.hasNext()) {
+                            HRow r = (HRow) it.next();
+                            if (!actName.equals(r.dis())) {
+                                continue;
+                            }
+                            String[] pSplit = Arrays.copyOf(split, split.length - 1);
+                            String parent = StringUtils.join(pSplit, "/");
+                            Node node = manager.getNode(parent, true).getNode();
+                            InvokeActions.handleAction(haystack, id, node, r);
+                            doThrow = false;
+                            container.node = node;
+                            break;
+                        }
+                        if (doThrow) {
+                            String err = "Action " + actName + " does not exist";
+                            throw new RuntimeException(err);
+                        }
+                        latch.countDown();
+                    }
+                });
+                break;
+            }
+        }
+        try {
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return container.node;
+    }
+
     public static void main(String[] args) {
         DSLinkFactory.start(args, new Main());
+    }
+
+    private static class Container {
+        Node node;
     }
 }
